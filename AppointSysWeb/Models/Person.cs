88d76bff9a8d36078c0a12b7967d6a4038ac4295
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-23 14:21:00 -0500

    feat: I added person model

---
+++ AppointSysWeb/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace AppointSysWeb.Models;
+
+public abstract class Person(
+    int id,
+    string firstName,
+    string lastName,
+    string email,
+    string phone,
+    string document,
+    string photo)
+{
+    public int Id { get; set; } = id;
+    public string FirstName { get; set; } = firstName.ToLower().Trim();
+    public string LastName { get; set; } = lastName.ToLower().Trim();
+    public string Email { get; set; } = email.ToLower().Trim();
+    public string Phone { get; set; } = phone.ToLower().Trim();
+    public string Document { get; set; } = document;
+    public string Photo { get; set; } = photo;
+}
